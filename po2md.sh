eea7a0980d049fa6e3675eca72eb1579ab02cfd1
Kopilov Aleksandr <kopilov.ad@gmail.com>
2014-09-14 22:09:15 +0400

    start support

--- po2md.sh
+++ po2md.sh
@@ -2,13 +2,13 @@
 java po2md.Po2md <xAPI_ru.po >xAPI_ru.md
 sed -i 's/ --/ –/g' xAPI_ru.md
 sed -i 's/-- /– /g' xAPI_ru.md
-#ln xAPI.md xAPI_en.md
-##A=$(md5sum xAPI_en.md)
-#rm xAPI_en.md
-#java po2md.Po2md checkorig <xAPI_ru.po >xAPI_en.md
-#B=$(md5sum xAPI_en.md)
-#if [[ $A != $B ]]; then
-#	echo "English output do not concur with original!"
-#else
-#	rm xAPI_en.md
-#fi
+ln xAPI.md xAPI_en.md
+A=$(md5sum xAPI_en.md)
+rm xAPI_en.md
+java po2md.Po2md checkorig <xAPI_ru.po >xAPI_en.md
+B=$(md5sum xAPI_en.md)
+if [[ $A != $B ]]; then
+	echo "English output do not concur with original!"
+else
+	rm xAPI_en.md
+fi
